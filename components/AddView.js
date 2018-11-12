5b1da861b6286d2b3eaacbeebb1518d1f8642aef
Erica Luong <huongie2lu@gmail.com>
2018-11-11 23:21:49 -0500

    Feature:Image Carousel

--- components/AddView.js
+++ components/AddView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Platform, StyleSheet, View, FlatList, Image} from 'react-native';
+import {Platform, StyleSheet, View, FlatList, Image, Animated, Dimensions, ScrollView} from 'react-native';
 import { Container, Header, Content, Icon, Text, Button } from 'native-base';
 import { NavigationActions } from 'react-navigation';
 import {connect} from 'react-redux'
@@ -9,7 +9,11 @@ import axios from 'axios'
 
 var ImagePicker = require('react-native-image-picker')
 
-var cookieMonster = require('./img/cookie_monster.png')
+var milkCookies = require('./img/milk_cookies.jpg')
+var milkGlass = require('./img/milk_glass.jpg')
+var milkMuffin= require('./img/milk_muffin.jpg')
+var milkKiwi = require('./img/milk_kiwi.jpg')
+var milkDonut = require('./img/milk_donut.jpg')
 
 
 const API_KEY = 'AIzaSyBWm5aQTSWyffTokyKlsX03j7d9hIgH7Q0'
@@ -70,56 +74,56 @@ class AddView extends Component {
 
   //Sends receipt image that user picks to the Google Cloud Vision OCR API which returns an object with the text
   // Loop through the object to find if it contains "milk", if it does, invoke the postItem function
-  getOCR =  async () => {
-    let body = {
-      "requests": [
-        {
-          "image": {
-            //USE FOR PUBLIC URLS OR GOOGLE CLOUD STORED IMAGES
-            // "source": {
-            //   "imageUri": "https://firebasestorage.googleapis.com/v0/b/grocerlert.appspot.com/o/milk_receipt_2.jpg?alt=media&token=8c4e577e-14fd-44c5-b56e-7e526eb00257" //image URL
-            // }
-            "content":this.state.base64
-          },
-          "features": [
-            {
-              "type": "TEXT_DETECTION",
-              "maxResults": 1
-            }
-          ]
-        }
-      ]
-    }
-    try {
-      let response = await axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`,
-       body)
-      console.log('GOOGLE', response)
-      let OCR_fullTextAnnotation = response.data.responses[0].fullTextAnnotation.text
-      console.log('GOOGLE FULL TEXT ANNOT', OCR_fullTextAnnotation)
-      let OCR_lowerCase = OCR_fullTextAnnotation.toLowerCase()
-      console.log('OCR LOWER CASE:', OCR_lowerCase)
-      if (OCR_lowerCase.includes('milk') || OCR_lowerCase.includes('mlk') ) {
-          console.log('OCR Found milk')
-          this.postItem()
-         }
-         else {
-           alert('No Milk Found On Your Receipt')
-         }
-   }
-   catch (error) {console.log(error)}
+  getOCR =  () => {
+  //   let body = {
+  //     "requests": [
+  //       {
+  //         "image": {
+  //           //USE FOR PUBLIC URLS OR GOOGLE CLOUD STORED IMAGES
+  //           // "source": {
+  //           //   "imageUri": "https://firebasestorage.googleapis.com/v0/b/grocerlert.appspot.com/o/milk_receipt_2.jpg?alt=media&token=8c4e577e-14fd-44c5-b56e-7e526eb00257" //image URL
+  //           // }
+  //           "content":this.state.base64
+  //         },
+  //         "features": [
+  //           {
+  //             "type": "TEXT_DETECTION",
+  //             "maxResults": 1
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  //   try {
+  //     let response = await axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`,
+  //      body)
+  //     console.log('GOOGLE', response)
+  //     let OCR_fullTextAnnotation = response.data.responses[0].fullTextAnnotation.text
+  //     console.log('GOOGLE FULL TEXT ANNOT', OCR_fullTextAnnotation)
+  //     let OCR_lowerCase = OCR_fullTextAnnotation.toLowerCase()
+  //     console.log('OCR LOWER CASE:', OCR_lowerCase)
+  //     if (OCR_lowerCase.includes('milk') || OCR_lowerCase.includes('mlk') ) {
+  //         console.log('OCR Found milk')
+  //         this.postItem()
+  //        }
+  //        else {
+  //          alert('No Milk Found On Your Receipt')
+  //        }
+  //  }
+  //  catch (error) {console.log(error)}
 
     //For Testing: when switching to OCR, put back async and await
-    //  let OCR_fullTextAnnotation = OCR_RESPONSE.data.responses[0].fullTextAnnotation.text
-    //  let OCR_lowerCase = OCR_fullTextAnnotation.toLowerCase()
-    //  console.log('Test_OCR_fullTextAnnotation:', OCR_fullTextAnnotation)
-    //  console.log('Test_OCR_lowerCase:', OCR_lowerCase)
-    //  if (OCR_lowerCase.includes('milk')) {
-    //   console.log('OCR Found milk')
-    //   this.postItem()
-    //   this.navigateToListView()
+     let OCR_fullTextAnnotation = OCR_RESPONSE.data.responses[0].fullTextAnnotation.text
+     let OCR_lowerCase = OCR_fullTextAnnotation.toLowerCase()
+     console.log('Test_OCR_fullTextAnnotation:', OCR_fullTextAnnotation)
+     console.log('Test_OCR_lowerCase:', OCR_lowerCase)
+     if (OCR_lowerCase.includes('milk')) {
+      console.log('OCR Found milk')
+      this.postItem()
+      this.navigateToListView()
 
-    //  }
-    //  else alert('No Milk Found On Your Receipt')
+     }
+     else alert('No Milk Found On Your Receipt')
   }
 
 //If Milk is on the receipt, post milk to the Firebase database w/ days til expiration from today's date
@@ -157,13 +161,47 @@ class AddView extends Component {
   }
 
   render() {
+    const deviceWidth = Dimensions.get('window').width
+
     return (
-      <View>
-      <Image source={cookieMonster} style={{
-        width:575,
+      <View style={styles.container}>
+        <ScrollView
+           horizontal //scrolling left to right instead of top to bottom
+          showsHorizontalScrollIndicator={false} //hides native scrollbar
+          pagingEnabled
+
+         >
+
+      <Image source={milkCookies} style={{
+        width:deviceWidth,
         height:575,
         resizeMode:'contain'}}/>
-        <Button bordered dark small primary style={{alignSelf: 'center', marginTop:5, marginleft:5, borderColor:'black'}} onPress={this.pickImage}>
+
+
+
+        <Image source={milkMuffin} style={{
+        width:deviceWidth,
+        height:575,
+        resizeMode:'contain'}}/>
+
+        <Image source={milkDonut} style={{
+        width:deviceWidth,
+        height:575,
+        resizeMode:'contain'}}/>
+
+        <Image source={milkGlass} style={{
+        width:deviceWidth,
+        height:575,
+        resizeMode:'contain'}}/>
+
+        <Image source={milkKiwi} style={{
+        width:deviceWidth,
+        height:575,
+        resizeMode:'contain'}}/>
+
+
+        </ScrollView>
+        <Button bordered dark small primary style={{alignSelf: 'center', marginBottom:20, marginleft:5, borderColor:'black'}} onPress={this.pickImage}>
             <Icon name='ios-camera' style={{color:'#5E4955', fontSize:20}} />
             <Text style={{color:'#5E4955', fontFamily:'Avenir', fontWeight: 'bold', fontSize:15}}>YES!</Text>
         </Button>
@@ -176,7 +214,9 @@ class AddView extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
